9c25f57595c8b2ba6f30b7e3516a7ec71c98d53a
sani <khursani8@gmail.com>
2016-10-03 20:35:27 +0800

    change design

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -1,9 +1,15 @@
 'use strict';
 
-export function routeConfig($urlRouterProvider, $locationProvider) {
+export function routeConfig($urlRouterProvider, $locationProvider,$mdThemingProvider) {
   'ngInject';
 
   $urlRouterProvider.otherwise('/');
 
   $locationProvider.html5Mode(true);
+  $mdThemingProvider
+    .theme('default')
+    .primaryPalette('blue')
+    .accentPalette('teal')
+    .warnPalette('red')
+    .backgroundPalette('grey');
 }
